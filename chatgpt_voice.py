7a3c25c4cb3b203bf412bdbc0f66837abd1f030b
Arjun Radha Krishnan <arjundortmund@gmail.com>
2024-03-01 23:32:51 +0100

    updated files
    Added chimes when listening and doesnt receive an input

--- chatgpt_voice.py
+++ chatgpt_voice.py
@@ -1,4 +1,5 @@
 import os
+from openai import OpenAI
 import openai
 from dotenv import load_dotenv
 import time
@@ -7,20 +8,24 @@ import pyttsx3
 import numpy as np
 from gtts import gTTS
 import subprocess
+from playsound import playsound
+
 mytext = 'Welcome to me'
 language = 'en'
 # from os.path import join, dirname
 # import matplotlib.pyplot as plt
 # ^ matplotlib is great for visualising data and for testing purposes but usually not needed for production
-openai.api_key='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'
+#openai.api_key= os.getenv("OPEN_API_KEY") #'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'
+client = OpenAI(api_key="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")
+
 load_dotenv()
 model = 'gpt-3.5-turbo'
 # Set up the speech recognition and text-to-speech engines
 r = sr.Recognizer()
-engine = pyttsx3.init("dummy")
+engine = pyttsx3.init()
 voice = engine.getProperty('voices')[1]
 engine.setProperty('voice', voice.id)
-name = "YOUR NAME HERE"
+name = "Arjun"
 greetings = [f"whats up master {name}",
              "yeah?",
              "Well, hello there, Master of Puns and Jokes - how's it going today?",
@@ -29,13 +34,13 @@ greetings = [f"whats up master {name}",
 
 # Listen for the wake word "hey pos"
 def listen_for_wake_word(source):
-    print("Listening for 'Hey'...")
+    print("Listening for 'Hello'...")
 
     while True:
         audio = r.listen(source)
         try:
             text = r.recognize_google(audio)
-            if "hey" in text.lower():
+            if "hello" in text.lower():
                 print("Wake word detected.")
                 engine.say(np.random.choice(greetings))
                 engine.runAndWait()
@@ -43,11 +48,31 @@ def listen_for_wake_word(source):
                 break
         except sr.UnknownValueError:
             pass
+        
+# def get_completion(prompt, model="gpt-3.5-turbo"):
+#     messages = [{"role": "user", "content": prompt}]
+#     response = openai.ChatCompletion.create(
+#         model=model,
+#         messages=messages,
+#         temperature=0, # this is the degree of randomness of the model's output
+#     )
+#     return response.choices[0].message["content"]
+
+
+def get_completion(prompt, model="gpt-3.5-turbo-0125"):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0
+    )
+    return response.choices[0].message.content
+
 # Listen for input and respond with OpenAI API
 def listen_and_respond(source):
-    print("Listening...")
-
+    playsound("listen_chime.mp3")
     while True:
+        print("Listening...")
         audio = r.listen(source)
         try:
             text = r.recognize_google(audio)
@@ -56,21 +81,37 @@ def listen_and_respond(source):
                 continue
 
             # Send input to OpenAI API
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"{text}"}])
-            response_text = response.choices[0].message.content
+            response_text = get_completion(text) #openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"{text}"}])
+            #response_text = response.choices[0].message.content
             print(response_text)
-            #myobj = gTTS(text = response_text, lang = language, slow = False)
-            #myobj.save("test.wav")
-            #os.system("aplay test.wav")
+            # myobj = gTTS(text = response_text, lang = language, slow = False)
+            # myobj.save("test.mp3")
+            # playsound('test.mp3')
+            # speech_file_path = "test.mp3"
+            # response = client.audio.speech.create(
+            #   model="tts-1",
+            #   voice="alloy",
+            #   input=response_text
+            # )
+            
+            #response.stream_to_file(speech_file_path)
             # Speak the response
-            print("speaking")
-            os.system("espeak ' "+response_text + "'")
+            #print("speaking")
+            #playsound(speech_file_path)
+            
+            #os.system("aplay test.wav")
+            
+            #os.system("espeak ' "+response_text + "'")
             engine.say(response_text)
             engine.runAndWait()
+            playsound("listen_chime.mp3")
 
             if not audio:
+                playsound("error.mp3")
+                print("test")
                 listen_for_wake_word(source)
         except sr.UnknownValueError:
+            playsound("error.mp3")
             time.sleep(2)
             print("Silence found, shutting up, listening...")
             listen_for_wake_word(source)
